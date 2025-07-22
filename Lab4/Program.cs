e7ef64d0618db66b9a67ccc75cd0718cc976cf00
S01250872 <S01250872@acad.tri-c.edu>
2025-07-22 20:32:15 +0000

    Add Lab4 project

---
+++ Lab4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Car
+{
+    public string model;
+    public string color;
+    public int year;
+
+    // Display car details
+    public void Display()
+    {
+        Console.WriteLine("Car Details:");
+        Console.WriteLine("Model: " + model);
+        Console.WriteLine("Color: " + color);
+        Console.WriteLine("Year: " + year);
+    }
+
+    // Problem 1
+    public void Start()
+    {
+        Console.WriteLine("The car is starting.");
+    }
+
+    // Problem 2
+    public void Drive(int miles)
+    {
+        Console.WriteLine($"The car drove {miles} miles.");
+    }
+
+    // Problem 3
+    public string GetDescription()
+    {
+        return $"{year} {color} {model}";
+    }
+
+    // Problem 4
+    public void Repaint(string newColor)
+    {
+        color = newColor;
+        Console.WriteLine($"The car has been repainted to {color}.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car myCar = new Car();
+        myCar.model = "Civic";
+        myCar.color = "Black";
+        myCar.year = 2020;
+
+        myCar.Display();
+        myCar.Start();
+        myCar.Drive(50);
+
+        string description = myCar.GetDescription();
+        Console.WriteLine(description);
+
+        myCar.Repaint("Red");
+        Console.WriteLine("Updated color: " + myCar.color);
+    }
+}
